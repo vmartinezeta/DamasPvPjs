f2f8ffa4da37050b997ff1a735b5616b74ed305e
Víctor Martínez <vito.marodriguez@gmail.com>
2025-07-08 21:00:51 -0600

    movimiento normal y reina lineal

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import IntroConEfecto from './Components/IntroConEfecto';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import IntroConEfecto from './Components/IntroConEfecto'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
